9e3f26a0f866f5a08778a5ef59f89c760b277352
Pospelov Evgeny <pospelov.genya@yandex.ru>
2024-01-25 14:56:31 +0500

    fix error in length word and path for static files

--- siteForServerDev/authorizationModule/models.py
+++ siteForServerDev/authorizationModule/models.py
@@ -37,7 +37,7 @@ class UserManager(BaseUserManager):
         return user
     
 class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(db_index=True, max_lenght=255, unique=True)
+    username = models.CharField(db_index=True, max_length=255, unique=True)
     email = models.EmailField(db_index=True, unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
--- siteForServerDev/siteForServerDev/settings.py
+++ siteForServerDev/siteForServerDev/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = BASE_DIR / 'staticCollected'
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/collectedStatic'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
